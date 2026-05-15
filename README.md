d6dd18ae4a3e5065da2dfb078e34959f8748a496
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-15 13:28:10 +0200

    README+LICENSE dazu

---
+++ README.md
@@ -0,0 +1,21 @@
+# Ionic-App "Buchabfrage" #
+
+<br>
+
+Diese Repository enthält ein [Ionic](https://ionicframework.com/)/Angular-Projekt
+für eine mobile App, die die Details für ein Buch anhand der eingegebenen
+[ISBN13](https://de.wikipedia.org/wiki/Internationale_Standardbuchnummer#ISBN_13)
+von einer Web-API abruft.
+
+<br>
+
+
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License) for the files in this repository.
+
+<br>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,78 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  /** ISBN-13 des Buches, die Nutzer eingegeben hat. */
+  public isbn13: string = "";
+
+  /** Flag um während einer Anfrage die Buttons zu sperren. */
+  public requestLaeuft: boolean = false;
+
+  /** Wert für Ergebnisfeld mit Vor- und Nachname des Autors. */
+  public autor: string = "";
+
+  /** Wert für Ergebnisfeld mit Titel des Buches. */
+  public titel: string = "";
+
+  /** Wert für Ergebnisfeld mit Verlag des Buches. */
+  public verlag: string = "";
+
+  /** Fehlermeldung für die Anzeige unter dem Eingabefeld. */
+  public fehlermeldung: string = "";
+
+
+  /**
+   * Konstruktor für *Dependency Injection*.
+   */
+  constructor( private httpClient: HttpClient ) {}
+
+
+  /**
+   * Event-Handler für den "Abrufen"-Button.
+   * Hier soll die Anfrage an die Web-API gestellt werden.
+   */
+  public async onAbrufenButton() {
+
+    this.fehlermeldung = "";
+    this.autor  = "";
+    this.titel  = "";
+    this.verlag = "";
+
+    this.requestLaeuft = true;
+
+    const isbn13trimmed = this.isbn13.trim();
+
+    if ( isbn13trimmed === "" ) {
+
+      this.fehlermeldung = "Bitte eine ISBN-13 eingeben.";
+      this.requestLaeuft = false;
+      return;
+    }
+
+    if ( isbn13trimmed.length !== 13 ) {
+
+      this.fehlermeldung = "Die ISBN-13 muss genau 13 Zeichen lang sein.";
+      this.requestLaeuft = false;
+      return;
+    }
+
+    this.fehlermeldung = "HTTP-Anfrage noch nicht implementiert :-(";
+    this.requestLaeuft = false;
+
+  }
+
+
+  /**
+   * Event-Handler für den "Löschen"-Button.
+   * Hier sollen die Eingabefelder zurückgesetzt werden.
+   */
+  public async onLoeschenButton() {
+
+    this.isbn13        = "";
+    this.fehlermeldung = "";
+
+    this.autor = "";
+    this.titel = "";
+    this.verlag = "";
+  }
 
 }
